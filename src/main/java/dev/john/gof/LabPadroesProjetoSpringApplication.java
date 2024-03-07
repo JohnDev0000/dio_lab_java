604dc42a4227cb50c26808d5472babfb858d385c
JohnDev0000 <johncroft2236@hotmail.com>
2024-03-07 13:07:15 -0300

    first commit

---
+++ src/main/java/dev/john/gof/LabPadroesProjetoSpringApplication.java
@@ -0,0 +1,26 @@
+package dev.john.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Projeto Spring Boot gerado via Spring Initializr.
+ * Os seguintes módulos foram selecionados:
+ * - Spring Data JPA
+ * - H2 Database
+ * - OpenFeign
+ *
+ * @author johnc
+ */
+
+
+@EnableFeignClients
+@SpringBootApplication
+public class LabPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+	}
+
+}
